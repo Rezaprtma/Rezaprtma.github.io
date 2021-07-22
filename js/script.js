a7271eb1a2510fb3fdccac23d941caf0107c7f38
rezaprtma <novianrezapratama@gmail.com>
2021-07-22 12:42:44 +0700

    memperbaiki  fitur

--- js/script.js
+++ js/script.js
@@ -19,26 +19,26 @@ setInterval(function () { countdown(); }, 1000);
 //   close();
 // }
 
-// Link Instagram
-// var instagram = document.getElementById("instagram").addEventListener("click", tampilkanAlertInstagram);
-// function tampilkanAlertInstagram() {
-//   var tes = confirm("Anda yakin ingin mengunjungi instagram?");
-//   if (tes) {
-//     window.open("https://instagram.com/__diamondlight__", "_blank");
-//   } else {
-//     ("");
-//   }
-//   return;
-// }
+Link Instagram
+var instagram = document.getElementById("instagram").addEventListener("click", tampilkanAlertInstagram);
+function tampilkanAlertInstagram() {
+  var tes = confirm("Anda yakin ingin mengunjungi instagram?");
+  if (tes) {
+    window.open("https://instagram.com/__diamondlight__", "_blank");
+  } else {
+    getElementById("home");
+  }
+  return;
+}
 
-// // Link Github
-// var instagram = document.getElementById("github").addEventListener("click", tampilkanAlertGithub);
-// function tampilkanAlertGithub() {
-//   var tes = confirm("Anda yakin ingin mengunjungi github?");
-//   if (tes) {
-//     window.open("https://github.com/Rezaprtma/Rezaprtma.github.io ", "_blank");
-//   } else {
-//     ("");
-//   }
-//   return;
-// }
+// Link Github
+var instagram = document.getElementById("github").addEventListener("click", tampilkanAlertGithub);
+function tampilkanAlertGithub() {
+  var tes = confirm("Anda yakin ingin mengunjungi github?");
+  if (tes) {
+    window.open("https://github.com/Rezaprtma/Rezaprtma.github.io ", "_blank");
+  } else {
+    getElementById("home");
+  }
+  return;
+}
